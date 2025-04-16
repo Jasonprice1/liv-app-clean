bc221757ea516f46c0d0c68703573ef7bc5dbede
Jasonprice1 <jasonbprice@hotmail.com>
2025-04-15 21:03:18 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ChatMockup from './ChatMockup.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1 style={{ textAlign: 'center', marginTop: '50px' }}>It's working ✅</h1>
+    <ChatMockup />
   </React.StrictMode>
 );
