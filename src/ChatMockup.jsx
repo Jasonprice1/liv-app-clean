c342941becaf2225d7b1946888e2d97a4d546a98
Jasonprice1 <jasonbprice@hotmail.com>
2025-04-15 21:08:52 -0700

    Delete src directory

--- src/ChatMockup.jsx
+++
@@ -1,8 +0,0 @@
-export default function ChatMockup() {
-  return (
-    <div style={{ padding: '1rem' }}>
-      <p><strong>ai:</strong> hey u good?</p>
-      <p><strong>user:</strong> Not much, just pretending to text someone lol</p>
-    </div>
-  );
-}
